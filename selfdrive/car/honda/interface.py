6c4e8dc29a8f4e5f3e5601c6a0dacf9ac0b65ecb
DBCooper <genejockey@gmail.com>
2021-02-21 21:51:42 -0600

    Increase ret.lateralTuning.pid.kf

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -147,7 +147,8 @@ class CarInterface(CarInterfaceBase):
     # For modeling details, see p.198-200 in "The Science of Vehicle Dynamics (2014), M. Guiggiani"
     ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0], [0]]
     ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
-    ret.lateralTuning.pid.kf = 0.00006  # conservative feed-forward
+    # ret.lateralTuning.pid.kf = 0.00006  # conservative feed-forward
+    ret.lateralTuning.pid.kf = 0.000075  # dbcc
 
     eps_modified = True
     for fw in car_fw:
